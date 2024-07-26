aa28ef3f14d590643659481482a2d1282c7cb8fe
AbdulMunnam07 <createrking40@gmail.com>
2024-07-26 12:01:23 +0500

    Check

--- Hacker Terminal/script.js
+++ Hacker Terminal/script.js
@@ -1,113 +1,38 @@
-const func1 = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Initializing...");
-      }, 2000);
-    });
-  };
+let additems = async (items) => {
+  await randomDely();
+  let div = document.createElement("div")
+  div.innerHTML = items;
+  document.body.append(div)
+}
+
+let randomDely = () => {
+  return new Promise((resolve, reject) => {
+    let timeout = 1 + 6* Math.random();
+    setTimeout(() => {
+      resolve()
+    }, timeout*1000);
+  })
   
-  func1()
-    .then((e) => {
-      const new_ele1 = document.createElement("h3");
-      new_ele1.innerText = e;
-      document.body.append(new_ele1);
-      return "Reading files...";
-    })
-    .then((e) => {
-      console.log(e); 
-    });
+}
 
-
-const func2 = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Initializing...");
-      }, 2000);
-    });
-  };
+async function main() {
   
-  func2()
-    .then((e) => {
-      const new_ele2 = document.createElement("h3");
-      new_ele2.innerText = e;
-      document.body.append(new_ele2);
-      return "Reading files...";
-    })
-    .then((e) => {
-      console.log(e); 
-    });
-
-
-const func3 = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Initializing...");
-      }, 2000);
-    });
-  };
+  setInterval(() => {
+    let last = document.body.getElementsByTagName("div");
+    last = last[last.length - 1]
+    if (last.innerHTML.endsWith("...")){
+      last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length - 3);
+    }
+    else{
+      last.innerHTML = last.innerHTML + "."
+    }
+  }, 300);
   
-  func3()
-    .then((e) => {
-      const new_ele3 = document.createElement("h3");
-      new_ele3.innerText = e;
-      document.body.append(new_ele3);
-      return "Reading files...";
-    })
-    .then((e) => {
-      console.log(e); 
-    });
-
-
-const func4 = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Initializing...");
-      }, 2000);
-    });
-  };
+  let text = ["Initializing Hacking", "Password files detected", "Sending all private files and public files to the server", "Reaeding all data", "Cleaning Up", "You got Hacked!!!", "Continue"]
   
-  func4()
-    .then((e) => {
-      const new_ele4 = document.createElement("h3");
-      new_ele4.innerText = e;
-      document.body.append(new_ele4);
-      return "Reading files...";
-    })
-    .then((e) => {
-      console.log(e); 
-    });
+for (const items of text) {
+  await additems(items)
+}
 
-
-const func5 = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Initializing...");
-      }, 2000);
-    });
-  };
-  
-  func5()
-    .then((e) => {
-      const new_ele5 = document.createElement("h3");
-      new_ele5.innerText = e;
-      document.body.append(new_ele5);
-      return "Reading files...";
-    })
-    .then((e) => {
-      console.log(e); 
-    });
-
-
-// let new_ele2 = document.createElement("h3");
-// setTimeout(() => {
-//     new_ele2.innerText = "Reading files...";
-// }, 1500);
-// let new_ele3 = document.createElement("h3");
-// setTimeout(() => {
-//     new_ele3.innerText = "Processing files...";
-// }, 2000);
-// let new_ele4 = document.createElement("h3");
-// setTimeout(() => {
-//     new_ele4.innerText = "Cleaning up...";
-// }, 2000);
-// document.body.append(new_ele, new_ele2, new_ele3, new_ele4);
+}           
+main();
